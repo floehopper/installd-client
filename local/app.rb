529a5ef96da118e05bf7bab11e7a7c997c24d649
James Mead <james@floehopper.org>
2009-06-26 11:36:34 +0100

    Removed dependency on Hpricot and ActiveResource. Also solved problem with some apps needing sync to run with sudo privileges.

--- local/app.rb
+++ local/app.rb
@@ -1,5 +1,4 @@
 require 'fileutils'
-require 'hpricot'
 
 module Local
   
@@ -23,6 +22,7 @@ module Local
       apps_pattern = File.join(ENV['HOME'], 'Music', 'iTunes', 'Mobile Applications', '*.ipa')
       
       app_names = {}
+      doc = ''
       Dir[apps_pattern].each do |app_file|
         app_name = File.basename(app_file, '.ipa')
         unzip_dir = File.join('unzipped', app_name)
@@ -32,17 +32,24 @@ module Local
         `unzip -d "#{unzip_dir}" "#{app_file}" iTunesMetadata.plist`
         `plutil -convert xml1 "#{plist}"`
         
-        doc = Hpricot::XML(File.read(plist))
+        File.open(plist) do |file|
+          file.readline
+          file.readline
+          doc << file.read
+        end
         
-        itemName = (doc/'plist'/'dict'/"key[text()='itemName']")[0].next_sibling.inner_text
-        itemId = (doc/'plist'/'dict'/"key[text()='itemId']")[0].next_sibling.inner_text
-        softwareIcon57x57URL = (doc/'plist'/'dict'/"key[text()='softwareIcon57x57URL']")[0].next_sibling.inner_text
-        
-        app_names[itemName] = new('name' => itemName, 'item_id' => itemId, 'icon_url' => softwareIcon57x57URL)
+        # doc = Hpricot::XML(File.read(plist))
+        # 
+        # itemName = (doc/'plist'/'dict'/"key[text()='itemName']")[0].next_sibling.inner_text
+        # itemId = (doc/'plist'/'dict'/"key[text()='itemId']")[0].next_sibling.inner_text
+        # softwareIcon57x57URL = (doc/'plist'/'dict'/"key[text()='softwareIcon57x57URL']")[0].next_sibling.inner_text
+        # 
+        # app_names[itemName] = new('name' => itemName, 'item_id' => itemId, 'icon_url' => softwareIcon57x57URL)
       end
       
       FileUtils.rm_rf('unzipped')
       app_names
+      
     end
     
   end
