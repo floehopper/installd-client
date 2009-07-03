b4b91be63e277f2e5eb437dc5bd7a47365305bdd
James Mead <james@floehopper.org>
2009-07-03 10:54:47 +0100

    First stab at a RubyCocoa app to sync iPhone installs with webapp.

---
+++ mac/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  installd
+//
+//  Created by James Mead on 30/06/2009.
+//  Copyright (c) 2009 Floehopper Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
