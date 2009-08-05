4299e101a04813a3be6da3e12edad1a171b77b21
James Mead <james@floehopper.org>
2009-08-05 11:17:50 +0100

    James Adam had an app (Colloquy) which he had compiled locally and had not come from the App Store. This app did not have a iTunesMetadata.plist file and an exception was being raised and not handled. We now rescue the exception and log it. We also redirect stderr to stdout to capture useful error messages from the shell commands.

--- mac/sync.rb
+++ mac/sync.rb
@@ -19,21 +19,29 @@ class Sync
       
       doc = ''
       app_files.each_with_index do |app_file, index|
-        app_name = File.basename(app_file, '.ipa')
-        unzip_dir = File.join('/tmp/installd/unzipped', app_name)
-        plist = File.join(unzip_dir, 'iTunesMetadata.plist')
+        begin
+          app_name = File.basename(app_file, '.ipa')
+          unzip_dir = File.join('/tmp/installd/unzipped', app_name)
+          plist = File.join(unzip_dir, 'iTunesMetadata.plist')
         
-        execute %{/usr/bin/unzip -d "#{unzip_dir}" "#{app_file}" iTunesMetadata.plist}
-        execute %{/bin/chmod +rw "#{plist}"}
-        execute %{/usr/bin/plutil -convert xml1 "#{plist}"}
+          execute %{/usr/bin/unzip -d "#{unzip_dir}" "#{app_file}" iTunesMetadata.plist}
+          execute %{/bin/chmod +rw "#{plist}"}
+          execute %{/usr/bin/plutil -convert xml1 "#{plist}"}
         
-        File.open(plist) do |file|
-          file.readline
-          file.readline
-          doc << file.read
+          File.open(plist) do |file|
+            file.readline
+            file.readline
+            doc << file.read
+          end
+        
+          yield index if block_given?
+        rescue => exception
+          NSLog("Exception handled: #{exception}")
+          exception.backtrace.each do |line|
+            NSLog("  #{line}")
+          end
+          NSLog("Skipping: #{app_file}")
         end
-        
-        yield index if block_given?
       end
       
       FileUtils.rm_rf('/tmp/installd/unzipped')
@@ -59,12 +67,16 @@ class Sync
       
       NSLog("*** Sync ends ***")
     rescue => exception
-      NSLog(exception)
+      NSLog("Exception handled: #{exception}")
+      exception.backtrace.each do |line|
+        NSLog("  #{line}")
+      end
+      NSLog("*** Sync failed ***")
     end
     
     def execute(command)
       NSLog(command)
-      NSLog(`#{command}`)
+      NSLog(`#{command} 2>&1`)
       unless $?.success?
         message = "Error executing command: #{command}"
         NSLog(message)
