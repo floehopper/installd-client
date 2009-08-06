f10aca6ddf8c7f1ad5390c1bf9c647b500345767
James Mead <james@floehopper.org>
2009-08-06 09:38:03 +0100

    Converted instance variables to local variables.

--- mac/sync_controller.rb
+++ mac/sync_controller.rb
@@ -18,19 +18,19 @@ class SyncController < OSX::NSObject
   ib_outlets :username, :password, :hoursBetweenSyncs, :autoLaunch, :menu, :preferencesWindow
   
   def awakeFromNib
-    @status_bar = NSStatusBar.systemStatusBar
-    @status_item = @status_bar.statusItemWithLength(NSVariableStatusItemLength)
-    @status_item.setHighlightMode(true)
-    @status_item.setMenu(@menu)
+    status_bar = NSStatusBar.systemStatusBar
+    status_item = status_bar.statusItemWithLength(NSVariableStatusItemLength)
+    status_item.setHighlightMode(true)
+    status_item.setMenu(@menu)
     bundle = NSBundle.mainBundle
-    @app_icon = NSImage.alloc.initWithContentsOfFile(bundle.pathForResource_ofType('app', 'tiff'))
-    @app_alter_icon = NSImage.alloc.initWithContentsOfFile(bundle.pathForResource_ofType('app_a', 'tiff'))
-    @status_item.setImage(@app_icon)
-    @status_item.setAlternateImage(@app_alter_icon)
+    app_icon = NSImage.alloc.initWithContentsOfFile(bundle.pathForResource_ofType('app', 'tiff'))
+    app_alter_icon = NSImage.alloc.initWithContentsOfFile(bundle.pathForResource_ofType('app_a', 'tiff'))
+    status_item.setImage(app_icon)
+    status_item.setAlternateImage(app_alter_icon)
     
     @last_sync_item = NSMenuItem.alloc.init
     @last_sync_item.title = 'Not yet synced'
-    @status_item.menu.insertItem_atIndex(@last_sync_item, 0)
+    status_item.menu.insertItem_atIndex(@last_sync_item, 0)
     
     @preferences = Preferences.new
     
