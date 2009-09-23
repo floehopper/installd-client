8bfabfbb85f3f27dbcb9926a31b1fcfac2b1c7f3
James Mead <james@floehopper.org>
2009-09-23 16:37:12 +0100

    Remove copyright comment headers.

--- mac-prefpane/InstalldPref.m
+++ mac-prefpane/InstalldPref.m
@@ -1,11 +1,3 @@
-//
-//  InstalldPref.m
-//  Installd
-//
-//  Created by James Mead on 19/09/2009.
-//  Copyright (c) 2009 Floehopper Ltd. All rights reserved.
-//
-
 @interface InstalldLoader : NSObject
 {}
 @end
