26d7615d3bc4bebbb7aee7615d09ba39962ff16d
James Mead <james@floehopper.org>
2009-09-27 17:52:54 +0100

    Removed redundant code from SyncConnection. Extracted synchronous NSConnection call into SyncConnection and raise exceptions on error.

--- mac-prefpane/sync.sh
+++ mac-prefpane/sync.sh
@@ -9,39 +9,33 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'iphone_apps'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'sync_connection'))
 
 bundle_identifier = 'com.floehopper.installdPrefPane'
-@settings = Installd::Settings.new(bundle_identifier)
+settings = Installd::Settings.new(bundle_identifier)
 
 begin
-  iphone_apps = Installd::IphoneApps.new(@settings.itunes_directory)
-  doc = iphone_apps.extract_data
+  iphone_apps = Installd::IphoneApps.new(settings.itunes_directory)
+  data = iphone_apps.extract_data
   timestamp = Time.now.getlocal.strftime('%H:%M %a %d %B')
   
   NSLog("*** Sync begins ***")
-  @connection = Installd::SyncConnection.new
-  request = @connection.build_request(@settings.username, @settings.password, doc)
-  data, response, error = NSURLConnection.sendSynchronousRequest_returningResponse_error(request)
-  if error
-    NSLog("*** Sync error: #{error.localizedDescription}")
-  elsif (response.statusCode.to_s == '200')
-    NSLog("*** Sync ends ***")
-    @settings.last_sync_status = "Last synced #{timestamp}"
-  else
-    NSLog("*** Sync failed: #{response.statusCode} ***")
-    @settings.last_sync_status = "Sync failed #{timestamp}"
-  end
-  @settings.save
-  NSLog(@settings.last_sync_status)
+  connection = Installd::SyncConnection.new(settings.username, settings.password)
+  connection.synchronize(data)
+  NSLog("*** Sync ends ***")
   
-  center = NSDistributedNotificationCenter.defaultCenter
-  center.postNotificationName_object_userInfo_deliverImmediately(
-    "InstalldSyncDidComplete",
-    bundle_identifier,
-    { 'status' => @settings.last_sync_status },
-    true
-  )
+  settings.last_sync_status = "Last synced #{timestamp}"
 rescue => exception
+  settings.last_sync_status = "Sync failed #{timestamp}"
   NSLog("*** Sync failed with exception: #{exception} ***")
   exception.backtrace.each do |line|
     NSLog("  #{line}")
   end
 end
+
+settings.save
+
+center = NSDistributedNotificationCenter.defaultCenter
+center.postNotificationName_object_userInfo_deliverImmediately(
+  "InstalldSyncDidComplete",
+  bundle_identifier,
+  { 'status' => settings.last_sync_status },
+  true
+)
--- mac-prefpane/sync_connection.rb
+++ mac-prefpane/sync_connection.rb
@@ -6,23 +6,25 @@ module Installd
   class SyncConnection
   
     include OSX
-  
-    attr_accessor :on_success, :on_failure
-  
-    def build_request(username, password, doc)
-      NSLog("username: #{username}")
-      NSLog("password: #{password}")
+    
+    def initialize(username, password)
+      @username, @password = username, password
+    end
+    
+    def build_request(doc)
+      NSLog("username: #{@username}")
+      NSLog("password: #{@password}")
       host = ENV['LOCAL'] ? "installd.local" : "installd.com"
       NSLog("Using host: #{host}")
     
-      params_hash = { '_method' => 'put', 'doc' => doc }
+      params_hash = { '_method' => 'put', 'doc' => @doc }
       params = params_hash.inject('') { |v,i| v << "#{i[0].to_s}=#{CGI.escape(i[1].to_s)}&"}.chop
       params_data = NSString.stringWithString(params).dataUsingEncoding(NSASCIIStringEncoding)
     
-      url = NSURL.URLWithString("https://#{host}/users/#{username}/installs/synchronize")
+      url = NSURL.URLWithString("https://#{host}/users/#{@username}/installs/synchronize")
     
       request = NSMutableURLRequest.requestWithURL_cachePolicy_timeoutInterval(url, NSURLRequestUseProtocolCachePolicy, 30.0)
-      credentials = ["#{username}:#{password}"].pack('m').chomp
+      credentials = ["#{@username}:#{@password}"].pack('m').chomp
       request.addValue_forHTTPHeaderField("Basic #{credentials}", 'Authorization')
       request.setHTTPMethod('put')
       request.setHTTPBody(params_data)
@@ -30,47 +32,14 @@ module Installd
       request.addValue_forHTTPHeaderField('application/x-www-form-urlencoded', 'Content-Type')
       request
     end
-  
-    def send_data(username, password, doc)
-      request = build_request(username, password, doc)
-      @redirectCount = 0
-      @connection = NSURLConnection.alloc.initWithRequest_delegate(request, self)
-      unless @connection
-        raise 'NSURLConnection could not create connection'
-      end
+    
+    def synchronize(doc)
+      request = build_request(doc)
+      data, response, error = NSURLConnection.sendSynchronousRequest_returningResponse_error(request)
+      raise "Sync error: #{error.localizedDescription}" if error
+      raise "Sync failed: #{response.statusCode}" unless (response.statusCode.to_s == '200')
     end
-  
-    def connection_willSendRequest_redirectResponse(connection, request, redirectResponse)
-      NSLog('*** connection_willSendRequest_redirectResponse ***')
-      @redirectCount += 1
-      request
-    end
-  
-    def connection_didReceiveResponse(connection, response)
-      NSLog('*** connection_didReceiveResponse ***')
-      @statusCode = response.statusCode
-    end
-  
-    def connectionDidFinishLoading(connection)
-      NSLog('*** connectionDidFinishLoading ***')
-      NSLog("*** statusCode: #{@statusCode}") if @statusCode
-      NSLog("*** redirectCount: #{@redirectCount}") if @redirectCount
-      @connection = nil
-      # For some reason (presumably to do with HTTP Auth), there is one 'redirect' even on a successful sync
-      if (@statusCode.to_s == '200') && (@redirectCount <= 1)
-        on_success.call
-      else
-        on_failure.call
-      end
-    end
-  
-    def connection_didFailWithError(connection, error)
-      NSLog('*** connection_didFailWithError ***')
-      NSLog("*** error: #{error.localizedDescription}")
-      @connection = nil
-      on_failure.call
-    end
-  
+    
   end
 
 end
