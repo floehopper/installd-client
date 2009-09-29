1f112780bbc2ae53d2eb4fc72c961aebed2719d7
James Mead <james@floehopper.org>
2009-09-29 16:19:49 +0100

    Added a boolean flag indicating whether or not the preference pane has been launched before so that we can decided whether to try to migrate the preferences from the old App.

--- mac-prefpane/preferences.rb
+++ mac-prefpane/preferences.rb
@@ -9,6 +9,7 @@ module Installd
     attr_accessor :username
     attr_accessor :itunes_directory
     attr_accessor :last_sync_status
+    attr_accessor :launched_before
   
     def initialize(bundle_identifier)
       @bundle_identifier = bundle_identifier
@@ -16,22 +17,24 @@ module Installd
     end
   
     def load
-      @username = get_value('username', '')
-      @itunes_directory = get_value('itunes_directory', File.join(ENV['HOME'], 'Music', 'iTunes'))
-      @last_sync_status = get_value('last_sync_status', 'Not yet synced')
+      @username = get_value('username', '').to_s
+      @itunes_directory = get_value('itunes_directory', File.join(ENV['HOME'], 'Music', 'iTunes')).to_s
+      @last_sync_status = get_value('last_sync_status', 'Not yet synced').to_s
+      @launched_before = get_value('launched_before', false)
     end
   
     def save
       set_value('username', @username)
       set_value('itunes_directory', @itunes_directory)
       set_value('last_sync_status', @last_sync_status)
+      set_value('launched_before', NSNumber.numberWithBool(@launched_before))
       synchronize
     end
   
     private
-  
+    
     def get_value(key, default)
-      (CFPreferencesCopyAppValue(key, @bundle_identifier) || default).to_s
+      (CFPreferencesCopyAppValue(key, @bundle_identifier) || default)
     end
   
     def set_value(key, value)
